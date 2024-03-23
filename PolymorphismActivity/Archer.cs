2885bb3b8dc610376c407c6d9e16e4b3397b3f51
DrinSom <151276516+DrinSom@users.noreply.github.com>
2024-03-23 09:30:45 +0800

    Polymorphism Activity

---
+++ PolymorphismActivity/Archer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Adventurer
+{
+    public override void Class()
+    {
+        base.Class();
+        Debug.Log("1st Job: Archer");
+    }
+    
+}
---
+++ PolymorphismActivity/Crossbow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Crossbow : Archer
+    {
+       public override void Class()
+        {
+            base.Class();
+            Debug.Log("2nd Job: Crossbow");
+        }
+    }
